2d24e2a45eb3ac4bb3dcf85b1897d30655c62aa3
Colin Breakell <cbreakell99@gmail.com>
2024-01-30 20:40:12 +0000

    Added js so that when user clicks start it hides the strart screen and shows the question screen

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('start_game').addEventListener('click', function() {
+    document.getElementById('start_screen').classList.add('hide');
+    document.getElementById('main').classList.remove('hide');
+});
