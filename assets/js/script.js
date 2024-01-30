7afbfb86630470ce4b8f5f604044655f5b3b2a3f
Colin Breakell <cbreakell99@gmail.com>
2024-01-30 23:06:48 +0000

    Added questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,40 @@
+// Start screen to Question screen
 document.getElementById('start_game').addEventListener('click', function() {
     document.getElementById('start_screen').classList.add('hide');
     document.getElementById('main').classList.remove('hide');
+    document.getElementById('scores_area').classList.remove('hide');
 });
+
+const questions = [
+    {
+      question: "What house is Harry in?",
+      choices: ["Griffindor", "Slytherin", "Hufflepuff", "Ravenclaw"],
+      answer: "Griffindor",
+    },
+
+    {
+      question: "Name the killing curse?",
+      choices: ["Lumos", "Expelliarmus", "Avada Kedavra", "Levioso"],
+      answer: "Avada Kedavra",
+    },
+
+    {
+      question: "What is Harrys owl called?",
+      choices: ["Hedwig", "Scabbers", "Nagini", "Ted"],
+      answer: "Hedwig",
+    },
+
+    {
+      question: "Who is Draco malfoys dad?",
+      choices: ["Sirius", "Dumbledore", "Voldemort", "Lucius"],
+      answer: "Lucius",
+      },
+
+      {
+      question: "what relation was sirius black to Harry?",
+      choices: ["GrandFather", "GodFather", "Uncle", "Cousin"],
+      answer: "GodFather",
+      },
+
+  ]
+  
